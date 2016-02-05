b28bd291f1669800bb786fc0752fcd805003c916
Andrew Taber <andrew.e.taber@gmail.com>
2016-02-04 16:42:12 -0800

    Add clinton emails + first draft of parser.

---
+++ clinton_emails/parse.rb
@@ -0,0 +1,44 @@
+require 'csv'
+
+emails = CSV.open("Emails.csv")
+emails.shift
+
+parsed_messages = []
+
+while email = emails.shift do
+  body = email[-1]
+
+  parsed_message = []
+  # Remove inline metadata
+  remove_block = false
+  split = body.split("\n")[8..-9]
+  next unless split
+  split.each_with_index do |line, i|
+    # First remove classified blocks
+    if line =~ /CLASSIFIED/
+      remove_block = true
+    end
+
+    if remove_block
+      next
+    end
+
+    if line =~ /SUBJECT TO/
+      remove_block = false
+      next
+    end
+
+    if line =~ /RELEASE IN|(From|Sent|Re|To|Fwd|Fw|Attachments|For|mailto|Received|CC|Cc|Subject|Date)(:|;|,)|clintonemail.com|clintonernail.com|Case No|Doc No|Generating server:/
+      next
+    end
+
+    if line =~ /SOURCE|SUBJECT|Original Message|message headers:|AuditID:|CONFIDENTIAL|PART B|.docx|@state.gov/
+      next
+    end
+
+    parsed_message << line
+  end
+
+  puts parsed_message.join("\n")
+  parsed_messages << parsed_message.join("\n")
+end
